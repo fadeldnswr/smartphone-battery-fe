d67f494eb6455bf4b7f5281f15d71eb53ea673b1
fadeldnswr <fadel.achmad@ui.ac.id>
2025-11-07 15:21:00 +0700

    Create auth success page

---
+++ src/app/auth/success/page.tsx
@@ -0,0 +1,16 @@
+
+
+const SuccessPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#F9FAFB] text-center px-6">
+      <h1 className="text-3xl font-semibold text-green-600 mb-2">
+        Success
+      </h1>
+      <p className="text-gray-600 max-w-md">
+        Authentication successful! You can close this page.
+      </p>
+    </div>
+  )
+}
+
+export default SuccessPage
